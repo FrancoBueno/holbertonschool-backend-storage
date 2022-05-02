02e74fa30c4459aa2f10833ed79b42814e79860c
root <3290@holbertonschool.com>
2022-05-02 12:42:03 +0000

    task-3

---
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Lists all bands with Glam rock as their style
+
+SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC
