7c94c59f3935712c682699d9f2aecee29e753609
root <3290@holbertonschool.com>
2022-05-02 12:43:15 +0000

    task-3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,6 @@
 -- Lists all bands with Glam rock as their style
+-- ranked for their long
+-- asdasdasdassda
 
 SELECT band_name, ifnull(split, 2020)-ifnull(formed, 0) AS lifespan
 FROM metal_bands
