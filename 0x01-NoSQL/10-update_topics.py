eba68ecefb348348a3e6e686c0f824a34339c557
root <3290@holbertonschool.com>
2022-05-03 15:19:08 +0000

    task-10

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -5,7 +5,7 @@ import pymongo
 
 def update_topics(mongo_collection, name, topics):
     """ all topics of a school """
-    mongo_collection.update_Many(
-        {'name': name},
-        {'$set': {"topics": topics}}
-    )
+    query = {"name", name}
+    new = {"$set": {"topics": topics}}
+
+    mongo_collection.update_many(query, new)
