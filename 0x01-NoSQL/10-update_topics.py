d5168389b1b591e91854692a186169f4c1ed0162
root <3290@holbertonschool.com>
2022-05-03 15:14:30 +0000

    fix

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """function that changes all topics of a school document based on the name:"""
+import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
     """ all topics of a school """
-    mongo_collection.updateMany(
+    mongo_collection.update_Many(
         {'name': name},
         {'$set': {"topics": topics}}
     )
